dad2e9cde83df0afccab9288169162b3a46370db
goughjo02 <goughjo02@gmail.com>
2018-07-12 19:54:38 +0100

    add tests for function to trim data

---
+++ services/trim_data.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { trim_data } from './trim_data';
+
+describe('Data to trim incoming data', () => {
+	beforeEach(() => {
+		var data = [
+		{number: 1},
+		{number: 2},
+		{number: 3},
+		{number: 4},
+		{number: 5},
+		{number: 6},
+		{number: 7},
+		{number: 8},
+		{number: 9},
+		];
+	});
+	xit('range selector works', () => {
+		return false
+	});
+	xit('zoom selector works', () => {
+		return false
+	});
+	xit('combination of selectors works', () => {
+		return false
+	})
+})

445d78b976d488808147a34428a99e19594a96d1
goughjo02 <goughjo02@gmail.com>
2018-07-13 19:42:47 +0100

    write tests for http_reducer

--- redux/reducer.test.js
+++ redux/reducer.test.js
@@ -1,20 +1,129 @@
 import React from 'react';
+import deepFreeze from './deep-freeze';
 import renderer from 'react-test-renderer';
 
-import { data_reducer } from './';
+import { http_reducer } from './';
 import { LOADING, ERROR, SUCCESS } from './'
 
-describe('Data Reducer', () => {
-	xit('can change start range',  () => {
-
-	})
-	xit('can change end range', () => {
-
-	})
-	xit('can change start zoom', () => {
-
-	})
-	xit('can change end zoom', () => {
-
-	})
+describe('HTTP Reducer', () => {
+	it('should return the initial state', () => {
+    expect(http_reducer(undefined, {})).toEqual([
+      {
+        data: [],
+        iserror: false,
+        isloading: false
+      }
+    ])
+  })
+​
+  it('should handle LOADING', () => {
+    expect(
+      http_reducer([], {
+        type: LOADING,
+        isloading: true
+      })
+    ).toEqual([
+      {
+        data: [],
+        iserror: false,
+        isloading: true
+      }
+    ])
+​
+    expect(
+      reducer(
+        [
+          {
+            data: [],
+            iserror: false,
+            isloading: true
+          }
+        ],
+        {
+          type: LOADING,
+          isloading: false
+        }
+      )
+    ).toEqual([
+      {
+        data: [],
+        iserror: false,
+        isloading: false
+      }
+    ])
+  })
+​
+  it('should handle ERROR', () => {
+    expect(
+      http_reducer([], {
+        type: ERROR,
+        iserror: true
+      })
+    ).toEqual([
+      {
+        data: [],
+        iserror: true,
+        isloading: false
+      }
+    ])
+​
+    expect(
+      reducer(
+        [
+          {
+            data: [],
+            iserror: true,
+            isloading: false
+          }
+        ],
+        {
+          type: ERROR,
+          iserror: false
+        }
+      )
+    ).toEqual([
+      {
+        data: [],
+        iserror: false,
+        isloading: false
+      }
+    ])
+  })
+​
+  it('should handle SUCCESS', () => {
+    expect(
+      http_reducer([], {
+        type: SUCCESS,
+        data: ['test']
+      })
+    ).toEqual([
+      {
+        data: ['test'],
+        iserror: false,
+        isloading: false
+      }
+    ])
+​
+    expect(
+      reducer(
+        [
+          {
+            data: ['test'],
+            iserror: false,
+            isloading: false
+          }
+        ],
+        {
+          type: SUCCESS,
+          data: ['test 2']
+        }
+      )
+    ).toEqual([
+      {
+        data: ['test 2'],
+        iserror: false,
+        isloading: false
+      }
+    ])
+  })
 })

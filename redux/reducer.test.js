3718c5456e8cf751c1daf3534c228ece64414a1f
goughjo02 <goughjo02@gmail.com>
2018-07-13 19:03:38 +0100

    add files and tests

---
+++ redux/reducer.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { data_reducer } from './';
+import { LOADING, ERROR, SUCCESS } from './'
+
+describe('Data Reducer', () => {
+	xit('can change start range',  () => {
+
+	})
+	xit('can change end range', () => {
+
+	})
+	xit('can change start zoom', () => {
+
+	})
+	xit('can change end zoom', () => {
+
+	})
+})

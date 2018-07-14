d41ec6fa5385c6fdef7ea7b6f086ff199039f293
goughjo02 <goughjo02@gmail.com>
2018-07-14 13:20:48 +0100

    write reducr tests

--- redux/reducer.test.js
+++ redux/reducer.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import deepFreeze from "deep-freeze";
 import renderer from "react-test-renderer";
 
-import { http_data_reducer } from "./reducer";
-import { LOADING, ERROR, SUCCESS } from "./constants";
+import { http_data_reducer, range_reducer, zoom_reducer } from "./reducer";
+import { ERROR, LOADING, SUCCESS, START_RANGE, END_RANGE, START_ZOOM, END_ZOOM } from './constants';
 import { dataIsLoading, dataHasErrored, fetchDataSuccess } from './actions'
 
 describe("HTTP Reducer", () => {
@@ -91,3 +91,26 @@ describe("HTTP Reducer", () => {
 		expect(http_data_reducer(state_before, dataHasErrored(false))).toEqual(expected_result)
 	})
 });
+
+describe('range selectors', () => {
+	xit('returns initial state', () => {
+
+	})
+	xit('can set start range', () => {
+
+	})
+	xit('can set end range', () => {
+
+	})
+})
+describe('zoom selectors', () => {
+	xit('returns initial state', () => {
+		
+	})
+	xit('can set start zoom', () => {
+
+	})
+	xit('can set end zoom', () => {
+
+	})
+})

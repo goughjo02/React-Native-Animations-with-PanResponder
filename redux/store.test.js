06b71430ff71a1fe0ac015078f84e48bbe146cd5
goughjo02 <goughjo02@gmail.com>
2018-07-14 13:48:16 +0100

    add zoom and range into store and store tests passing

--- redux/store.test.js
+++ redux/store.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import deepFreeze from 'deep-freeze';
+import React from "react";
+import renderer from "react-test-renderer";
+import deepFreeze from "deep-freeze";
 
-import { configureStore } from './store';
+import { configureStore } from "./store";
 import {
 	dataHasErrored,
 	dataIsLoading,
@@ -13,10 +13,8 @@ import {
 	setEndZoom
 } from "./actions";
 
-
-describe('store', () => {
-
-	var store = configureStore()
+describe("store", () => {
+	var store = configureStore();
 
 	var default_state = {
 		data: {
@@ -32,69 +30,155 @@ describe('store', () => {
 			start: 0,
 			end: 100
 		}
-	}
-	deepFreeze(default_state)
-	it('can get state', () => {
-		expect(store.getState()).toBeTruthy()
-	})
-	it('initialises default state', () => {
-		expect(store.getState()).toEqual(default_state)
-	})
-	it('responds to data success', () => {
+	};
+	deepFreeze(default_state);
+	it("can get state", () => {
+		expect(store.getState()).toBeTruthy();
+	});
+	it("initialises default state", () => {
+		expect(store.getState()).toEqual(default_state);
+	});
+	it("responds to data success", () => {
 		const expected_state = {
 			data: {
-				data: ['test'],
+				data: ["test"],
 				isloading: false,
 				iserror: false
 			},
-		range: {
-			start: 0,
-			end: 0
-		},
-		zoom: {
-			start: 0,
-			end: 100
-		}
-		}
-		store.dispatch(fetchDataSuccess(['test']))
-		expect(store.getState()).toEqual(expected_state)
-	})
-	it('sets loading to true', () => {
+			range: {
+				start: 0,
+				end: 0
+			},
+			zoom: {
+				start: 0,
+				end: 100
+			}
+		};
+		store.dispatch(fetchDataSuccess(["test"]));
+		expect(store.getState()).toEqual(expected_state);
+	});
+	it("sets loading to true", () => {
 		const expected_state = {
 			data: {
 				data: [],
 				isloading: true,
 				iserror: false
 			},
-		range: {
-			start: 0,
-			end: 0
-		},
-		zoom: {
-			start: 0,
-			end: 100
-		}
-		}
-		store.dispatch(dataIsLoading(true))
-		expect(store.getState()).toEqual(expected_state)
-	})
-	it('sets error to true', () => {
+			range: {
+				start: 0,
+				end: 0
+			},
+			zoom: {
+				start: 0,
+				end: 100
+			}
+		};
+		store.dispatch(dataIsLoading(true));
+		expect(store.getState()).toEqual(expected_state);
+	});
+	it("sets error to true", () => {
 		const expected_state = {
 			data: {
 				data: [],
 				isloading: false,
 				iserror: true
 			},
-		range: {
-			start: 0,
-			end: 0
-		},
-		zoom: {
-			start: 0,
-			end: 100
-		}
-		}
-		store.dispatch(dataHasErrored(true))
-		expect(store.getState()).toEqual(expected_state)
-	})
-})
+			range: {
+				start: 0,
+				end: 0
+			},
+			zoom: {
+				start: 0,
+				end: 100
+			}
+		};
+		store.dispatch(dataHasErrored(true));
+		expect(store.getState()).toEqual(expected_state);
+		//RESET STATE
+		store.dispatch(fetchDataSuccess([]))
+	});
+	it("sets start range", () => {
+		const choice = 50;
+		const expected_state = {
+			data: {
+				data: [],
+				isloading: false,
+				iserror: false
+			},
+			range: {
+				start: choice,
+				end: 0
+			},
+			zoom: {
+				start: 0,
+				end: 100
+			}
+		};
+		store.dispatch(setStartRange(choice));
+		expect(store.getState()).toEqual(expected_state);
+		store.dispatch(setStartRange(0));
+	});
+	it("sets end range", () => {
+		const choice = 50;
+		const expected_state = {
+			data: {
+				data: [],
+				isloading: false,
+				iserror: false
+			},
+			range: {
+				start: 0,
+				end: choice
+			},
+			zoom: {
+				start: 0,
+				end: 100
+			}
+		};
+		store.dispatch(setEndRange(choice));
+		expect(store.getState()).toEqual(expected_state);
+		store.dispatch(setEndRange(0))
+	});
+	it("sets start zoom", () => {
+		const choice = 50;
+		const expected_state = {
+			data: {
+				data: [],
+				isloading: false,
+				iserror: false
+			},
+			range: {
+				start: 0,
+				end: 0
+			},
+			zoom: {
+				start: choice,
+				end: 100
+			}
+		};
+		store.dispatch(setStartZoom(choice));
+		expect(store.getState()).toEqual(expected_state);
+		store.dispatch(setStartZoom(0));
+	});
+	it("sets end zoom", () => {
+		const choice = 50;
+		const expected_state = {
+			data: {
+				data: [],
+				isloading: false,
+				iserror: false
+			},
+			range: {
+				start: 0,
+				end: 0
+			},
+			zoom: {
+				start: 0,
+				end: choice
+			}
+		};
+		store.dispatch(setEndZoom(choice));
+		expect(store.getState()).toEqual(expected_state);
+		store.dispatch(setEndZoom(0));
+	});
+});

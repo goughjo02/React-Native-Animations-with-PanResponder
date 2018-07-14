65b156bfcd041a49a47538d29f3cfc4415a648b9
goughjo02 <goughjo02@gmail.com>
2018-07-14 13:12:00 +0100

    create action creators and constants

--- redux/actions.js
+++ redux/actions.js
@@ -1,4 +1,4 @@
-import { ERROR, LOADING, SUCCESS } from './constants';
+import { ERROR, LOADING, SUCCESS, START_RANGE, END_RANGE, START_ZOOM, END_ZOOM } from './constants';
 
 export function dataIsLoading(bool) {
     return {
@@ -17,4 +17,32 @@ export function fetchDataSuccess(data) {
         type: SUCCESS,
         data: data
     };
+}
+
+export function setStartRange(index) {
+    return {
+        type: START_RANGE,
+        index: index
+    }
+}
+
+export function setEndRange(index) {
+    return {
+        type: END_RANGE,
+        index: index
+    }
+}
+
+export function setStartZoom(percent) {
+    return {
+        type: START_ZOOM,
+        percent: percent
+    }
+}
+
+export function setEndZoom(percent) {
+    return {
+        type: END_ZOOM,
+        percent: percent
+    }
 }
--- redux/constants.js
+++ redux/constants.js
@@ -1,3 +1,7 @@
 export const ERROR = 'data loading has errored';
 export const LOADING = 'data is loading';
 export const SUCCESS = 'succesful fetch of data';
+export const START_RANGE = 'start range constant'
+export const END_RANGE = 'end range constant';
+export const START_ZOOM = 'start zoom constant';
+export const END_ZOOM = 'end zoom constant';

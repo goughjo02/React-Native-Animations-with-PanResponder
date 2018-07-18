d0d571e9685e081b7a50220937d4993092811c70
goughjo02 <goughjo02@gmail.com>
2018-07-18 18:41:59 +0100

    add file

---
+++ components/legend/legend.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Animated, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+class Legend extends React.Component {
+	constructor(props) {
+		
+	}
+}

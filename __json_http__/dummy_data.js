fc3268444300723f7edf9e0180444ff52ec6d056
goughjo02 <goughjo02@gmail.com>
2018-07-22 00:05:22 +0100

    start working on wrapping app in provider

--- __json_http__/dummy_data.js
+++ __json_http__/dummy_data.js
@@ -1,4 +1,4 @@
-export default const [
+export const faux_data = [
     {
         "bought": 0,
         "date": "2012-05-01 00:00:00+00:00",
--- pages/page_one.js
+++ pages/page_one.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native'
-import { connect } from 'react-redux'
-import { Legend, Line, RadialChart } from '../components';
-import { fetchData } from '../services';
+import React from "react";
+import { Text } from "react-native";
+import { connect } from "react-redux";
+import { faux_data } from "../__json_http__/dummy_data";
+import { Legend, Line, RadialChart } from "../components";
+import { fetchData } from "../services";
+import { fetchDataSuccess } from "../redux";
+import { combination_selector, convertDateTime } from "../services";
 
 class PageOne extends React.Component {
 	constructor(props) {
 		super(props);
-		fetchData("__json_http__/dummy_data.json");
+	}
+	componentDidMount() {
+		faux_data.forEach(e => {
+			e.date = convertDateTime(e.date);
+			console.log( typeof e.date)
+		});
+		this.props.dispatch(fetchDataSuccess(faux_data));
 	}
 	render() {
 		var { data } = this.props;
 		if (data.length > 0) {
-			return (<Line data={data} />)
-		} else return (
-			<Text>No data present</Text>
-		)
+			console.log(typeof data[0].date);
+		}
+		return <Text>No data present</Text>;
 	}
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
 	return {
-		data: state.data.data
-	}
-}
+		data: combination_selector(
+			state.data.data,
+			state.range.start,
+			state.range.end,
+			state.zoom.start,
+			state.zoom.end
+		)
+	};
+};
 
-const ConnectedPage = connect(mapStateToProps)(PageOne)
+const ConnectedPage = connect(mapStateToProps)(PageOne);
 
-export { ConnectedPage as PageOne }
+export { ConnectedPage as PageOne };

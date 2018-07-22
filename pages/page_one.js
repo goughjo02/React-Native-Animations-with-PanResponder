52d627d2489b07cb1c92254022f12c9ba5084de4
goughjo02 <goughjo02@gmail.com>
2018-07-22 12:01:58 +0100

    add style to QC page

--- pages/page_one.js
+++ pages/page_one.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
 import { faux_data } from "../__json_http__/dummy_data";
 import { Legend, Line, RadialChart } from "../components";
 import { fetchData } from "../services";
 import { fetchDataSuccess } from "../redux";
-import { combination_selector, convertDateTime } from "../services";
+import { combination_selector, convertDateTime, getSum } from "../services";
 
 class PageOne extends React.Component {
 	constructor(props) {
@@ -20,12 +20,43 @@ class PageOne extends React.Component {
 	}
 	render() {
 		var { data } = this.props;
+		var sumData = [];
+		var keys = ["produced", "used", "sold"];
+		for (var i = keys.length - 1; i >= 0; i--) {
+			sumData.push({
+				name: keys[i],
+				value: getSum(data, keys[i])
+			});
+		}
 		if (data.length > 0) {
-			return <Line data={data} />
+			return (
+				<React.Fragment>
+					<View style={styles.one}>
+						<RadialChart
+							data={sumData}
+							radius={45}
+							strokeWidth={6}
+							offsetFactor={2}
+						/>
+						<Legend data={sumData} />
+					</View>
+					<Line data={data} />
+				</React.Fragment>
+			);
 		}
 		return <Text>No data present</Text>;
 	}
 }
+
+const styles = StyleSheet.create({
+	one: {
+		flexDirection: "row",
+		flexWrap: "nowrap",
+		alignItems: "center",
+		alignSelf: "stretch",
+		justifyContent: "space-around"
+	}
+});
 
 const mapStateToProps = state => {
 	return {

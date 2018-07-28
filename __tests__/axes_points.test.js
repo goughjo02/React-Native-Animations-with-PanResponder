bf509045af152ba5a6e20310a3129e72390359ce
goughjo02 <goughjo02@gmail.com>
2018-07-28 12:29:59 +0100

    add functions and tests for y axis functions

--- __tests__/axes_points.test.js
+++ __tests__/axes_points.test.js
@@ -1,15 +1,20 @@
-import { get_no_x_points, get_x_points } from '../services/axes_points';
+import {
+	get_no_x_points,
+	get_x_points,
+	get_no_y_points,
+	get_y_points
+} from "../services/axes_points";
 
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-describe('get_x_points', () => {
+describe("get_x_points", () => {
 	let maxDist;
 	let width;
 	let dataArray;
 	let noPoints;
 	let result;
 	let expectation;
-	it('can get total number of x points', () => {
+	it("can get total number of x points", () => {
 		maxDist = 10;
 		width = 99;
 		expectation = 10;
@@ -21,16 +26,54 @@ describe('get_x_points', () => {
 		noPoints = get_no_x_points(maxDist, width);
 		expect(noPoints).toEqual(expectation);
 	});
-	it('can extract set of points', () => {
-		dataArray = [0,1,2,3,4,5,6,7,8,9];
-		noPoints = 10
-		expectation = [0,1,2,3,4,5,6,7,8,9];
+	it("can extract set of points", () => {
+		dataArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+		noPoints = 10;
+		expectation = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 		result = get_x_points(dataArray, noPoints);
 		expect(result).toEqual(expectation);
-		dataArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+		dataArray = [
+			0,
+			1,
+			2,
+			3,
+			4,
+			5,
+			6,
+			7,
+			8,
+			9,
+			10,
+			11,
+			12,
+			13,
+			14,
+			15,
+			16,
+			17,
+			18,
+			19
+		];
 		noPoints = 6;
-		expectation = [1,4,7,10,13,16,19];
+		expectation = [1, 4, 7, 10, 13, 16, 19];
 		result = get_x_points(dataArray, noPoints);
 		expect(result).toEqual(expectation);
-	})
-})
+	});
+});
+
+describe("get_y_points", () => {
+	let maxDist;
+	let height;
+	let dataArray;
+	let noPoints;
+	let result;
+	let expectation;
+	it("can get total number of y points", () => {
+		expect(true).toEqual(false);
+	});
+	it("can get array of y points", () => {
+		expect(true).toEqual(false
+
+			);
+	});
+});
--- services/axes_points.js
+++ services/axes_points.js
@@ -21,3 +21,39 @@ export const get_x_points = (dataArray, noPoints) => {
 	}
 	return newArray;
 };
+
+export const get_x_axes_points = (maxDist, width, dataArray) => {
+	let noPoints = get_no_x_points(maxDist, width);
+	let x_points = get_x_points(dataArray, noPoints);
+	return x_points;
+}
+
+export const get_no_y_points; = (maxDist, height) => {
+	var noPoints = Math.floor(height, maxDist) + 1;
+	return noPoints;
+}
+
+export const get_y_points = ( dataArray, noPoints ) => {
+	let newArray = [];
+	var clonedArray = JSON.parse(JSON.stringify(dataArray));
+	var avgDist = Math.floor(clonedArray.length / noPoints);
+	let modulo = clonedArray.length % noPoints;
+	let offset = Math.floor(modulo / 2);
+	if (offset > 0 ) {
+		var slicedArray = clonedArray.slice(offset)
+	} else {
+		var slicedArray = clonedArray;
+	}
+	for (var i = 0; i <= slicedArray.length - 1; i++) {
+		if (i % avgDist == 0) {
+			newArray.push(slicedArray[i]);
+		}
+	}
+	return newArray;
+}
+
+export const get_y_axes_points = (maxDist, height, dataArray) => {
+	let noPoints = get_no_y_points(maxDist, height);
+	let y_points = get_y_points(dataArray, noPoints);
+	return y_points;
+}

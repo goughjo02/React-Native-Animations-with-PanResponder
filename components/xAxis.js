a15618ee6f51936f015a5eb4362279f395cbde86
goughjo02 <goughjo02@gmail.com>
2018-07-28 13:56:05 +0100

    add x axis file

---
+++ components/xAxis.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ART } from 'react-native'
+const { Surface, Group, Shape } = ART;
+
+class XAxis extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<Surface>
+			</Surface>)
+	}
+}

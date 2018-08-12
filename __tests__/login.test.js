ff26565978945ec0ab11fb780deb723187b9c356
goughjo02 <goughjo02@gmail.com>
2018-08-12 18:57:20 +0100

    add Mock Async Storage test

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,1 +1,22 @@
+import 'react-native';
+import MockAsyncStorage from 'mock-async-storage'
 import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+const mock = () => {
+  const mockImpl = new MockAsyncStorage()
+  jest.mock('AsyncStorage', () => mockImpl)
+}
+
+mock();
+
+import { AsyncStorage as storage } from 'react-native'
+
+
+it('Mock Async Storage working', async () => {
+  await storage.setItem('myKey', 'myValue')
+  const value = await storage.getItem('myKey')
+  expect(value).toBe('myValue')
+})

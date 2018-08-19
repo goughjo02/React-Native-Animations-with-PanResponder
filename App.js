46f2e20108f7c2b8604b55c06d2c1951a75e5331
goughjo02 <goughjo02@gmail.com>
2018-08-19 01:59:40 +0100

    add redux persist

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { compose, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { createSwitchNavigator, createStackNavigator } from "react-navigation";
+import { persistStore, persistReducer } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
-import throttle from "lodash/throttle";
 
-import { root_reducer } from "./redux";
+
+
+// CONFIGURE REACT - NAVIGATION
 import { PageOne, AuthLoadingScreen, SignInScreen } from "./pages";
-import { loadJwt, saveJwt, NavigationService } from "./services";
-
+import { createSwitchNavigator, createStackNavigator } from "react-navigation";
 const AppStack = createStackNavigator({ Home: { screen: PageOne } });
 const AuthStack = createStackNavigator({ SignIn: { screen: SignInScreen } });
-
 const RootStack = createSwitchNavigator(
   {
     AuthLoading: AuthLoadingScreen,
@@ -24,31 +26,29 @@ const RootStack = createSwitchNavigator(
     initialRouteName: "AuthLoading"
   }
 );
+//--------------------------------------
 
-let store = createStore(root_reducer, applyMiddleware(thunk));
+// CONFIGURE REDUX STORE
+import { root_reducer } from "./redux";
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2
+};
+const persistedReducer = persistReducer(persistConfig, root_reducer);
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentWillMount() {
-    // let persistedState = loadJwt();
-    // this.store = createStore(
-    //   root_reducer,
-    //   applyMiddleware(thunk)
-    // );
-  }
-  componentDidMount() {
-    // this.store.subscribe(
-    //   throttle(() => {
-    //     saveJwt(this.store.getState());
-    //   }, 1000)
-    // );
-  }
   render() {
-    // console.log(NavigationService)
     return (
       <Provider store={store}>
-        <RootStack />
+        <PersistGate loading={null} persistor={persistor}>
+          <RootStack />
+        </PersistGate>
       </Provider>
     );
   }
